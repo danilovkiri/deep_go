32015d0912782d4f793f7aa80838723fb2c82600
Kirill Danilov <danilov.kirill13@wb.ru>
2025-05-05 19:39:21 +0400

    add homework 8

--- homework/errors/homework_test.go
+++ homework/errors/homework_test.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"errors"
+	"strconv"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -10,17 +12,36 @@ import (
 // go test -v homework_test.go
 
 type MultiError struct {
-	// need to implement
+	errs []error
 }
 
 func (e *MultiError) Error() string {
-	// need to implement
+	var errText strings.Builder
+	for _, subErr := range e.errs {
+		errText.WriteString("\t* " + subErr.Error())
+	}
+	if errText.Len() > 0 {
+		return strconv.Itoa(len(e.errs)) + " error(s) occurred:\n" + errText.String() + "\n"
+	}
+
 	return ""
 }
 
 func Append(err error, errs ...error) *MultiError {
-	// need to implement
-	return nil
+	var newErr MultiError
+	if err != nil {
+		if mErr, ok := err.(*MultiError); ok {
+			newErr.errs = append(newErr.errs, mErr.errs...)
+		} else {
+			newErr.errs = append(newErr.errs, err)
+		}
+	}
+	newErr.errs = append(newErr.errs, errs...)
+	return &newErr
+}
+
+func (e *MultiError) Unwrap() []error {
+	return e.errs
 }
 
 func TestMultiError(t *testing.T) {
@@ -28,6 +49,6 @@ func TestMultiError(t *testing.T) {
 	err = Append(err, errors.New("error 1"))
 	err = Append(err, errors.New("error 2"))
 
-	expectedMessage := "2 errors occured:\n\t* error 1\t* error 2\n"
+	expectedMessage := "2 error(s) occurred:\n\t* error 1\t* error 2\n"
 	assert.EqualError(t, err, expectedMessage)
 }
